0af327bb91122a2995b3f4dd487c541133fa2d81
sfuentes-dev <sfuentes.dev@gmail.com>
2023-02-03 14:51:32 -0500

    AuthProvider Updated

--- context/auth/AuthProvider.tsx
+++ context/auth/AuthProvider.tsx
@@ -2,10 +2,10 @@ import { FC, useReducer, useEffect } from 'react';
 import { useSession, signOut } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import axios from 'axios';
-import { tesloApi } from '@/api';
 import Cookies from 'js-cookie';
 import { IUser } from '@/interfaces';
 import { AuthContext, authReducer } from './';
+import { tesloApi } from '@/pages/api';
 
 export interface AuthState {
   isLoggedIn: boolean;
@@ -45,10 +45,7 @@ export const AuthProvider: FC<Props> = ({ children }) => {
     }
   };
 
-  const loginUser = async (
-    email: string,
-    password: string
-  ): Promise<boolean> => {
+  const loginUser = async (email: string, password: string): Promise<boolean> => {
     try {
       const { data } = await tesloApi.post('/user/login', { email, password });
       const { token, user } = data;
